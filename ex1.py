456f5e385832a493432186d418b381467761bdd1
Dipika <35023867+desdipika@users.noreply.github.com>
2018-01-12 11:18:18 +0530

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,7 +1,9 @@
 # Exercise 1, Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
 name = input("What is your name?\n")
 print("Hello there, "+name+"!")
 age = input("How old are you?\n")
 x = 100-int(age)
 print("You will be a 100 years old in "+str(x)+" years!\n")
-print("Thanks!")
+num = int(input("Give us a random number you like: "))
+print(num*("You will be a 100 years old in "+str(x)+" years!\n"))
